5083cda8fb1b3fd7292777648183dbaee0f10882
Andreas Sjövall <a.n.d.r.e.a.s_95@hotmail.com>
2018-03-23 15:56:11 +1300

    Create test for App

---
+++ src/App.spec.js
@@ -0,0 +1,15 @@
+describe('Local State', () => {
+    it('should increment the counter in state', () => {
+        const state = { counter: 0 };
+        const newState = doIncrement(state);
+
+        expect(newState.counter).to.equal(1);
+    });
+
+    it('should decrement the counter in state', () => {
+        const state = { counter: 0 };
+        const newState = doDecrement(state);
+
+        expect(newState.counter).to.equal(-1);
+    });
+});
